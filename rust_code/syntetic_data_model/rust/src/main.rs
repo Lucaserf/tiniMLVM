44b6601bcde5c3642270bd30010d0d158f17f5c1
Luca <luca.serfilippi@unibo.it>
2024-06-25 09:16:00 +0000

    rust optimization

--- rust_code/syntetic_data_model/rust/src/main.rs
+++ rust_code/syntetic_data_model/rust/src/main.rs
@@ -1,3 +1,4 @@
+use std::time::{Instant, SystemTime};
 use std::{env, error::Error, fs};
 use wasi_nn::{ExecutionTarget, GraphBuilder, GraphEncoding, TensorType};
 
@@ -7,35 +8,51 @@ pub fn main() -> Result<(), Box<dyn Error>> {
     let data_point_name: &str = &args[2];
 
     let weights = fs::read(model_bin_name)?;
-    println!("Read graph weights, size in bytes: {}", weights.len());
+    // println!("Read graph weights, size in bytes: {}", weights.len());
 
     let graph = GraphBuilder::new(GraphEncoding::TensorflowLite, ExecutionTarget::CPU)
         .build_from_bytes(&[&weights])?;
     let mut ctx = graph.init_execution_context()?;
-    println!("Loaded graph into wasi-nn with ID: {}", graph);
+    // println!("Loaded graph into wasi-nn with ID: {}", graph);
 
     //read data points from csv file
-    println!("Reading data points from csv file: {:?}", data_point_name);
+    // println!("Reading data points from csv file: {:?}", data_point_name);
+
+    println!("timestamp, inference_time [ns]");
     let mut rdr = csv::Reader::from_path(data_point_name)?;
-    let mut tensor_data = vec![];
     for result in rdr.records() {
+        let mut tensor_data = Vec::new();
         let record = result?;
-        println!("record: {:?}", record);
+        // println!("record: {:?}", record);
         for value in record.iter() {
             tensor_data.push(value.parse::<f32>()?);
         }
+        //delete the last element being the label
+        tensor_data.pop();
+
+        //print data points
+        // println!("tensor data: {:?}", tensor_data);
+        // println!("Read input tensor, size in bytes: {}", tensor_data.len());
+
+        let mut output_buffer = vec![0f32; 1];
+        //get inference time
+        let start = Instant::now();
+        ctx.set_input(0, TensorType::F32, &[1, 10], &tensor_data)?;
+        ctx.compute()?;
+        _ = ctx.get_output(0, &mut output_buffer)?;
+        let duration = start.elapsed();
+        println!(
+            "{}, {}",
+            SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_nanos(),
+            duration.as_nanos()
+        );
+        // println!("Inference time: {:?}", duration);
+
+        // println!("Output tensor: {:?}", output_buffer);
     }
-    //print data points
-    println!("tensor data: {:?}", tensor_data);
-    println!("Read input tensor, size in bytes: {}", tensor_data.len());
-    ctx.set_input(0, TensorType::F32, &[1, 10], &tensor_data)?;
-
-    ctx.compute()?;
-
-    let mut output_buffer = vec![0f32; 1];
-    _ = ctx.get_output(0, &mut output_buffer)?;
-
-    println!("Output tensor: {:?}", output_buffer);
 
     Ok(())
 }
