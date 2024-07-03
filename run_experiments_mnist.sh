8cb06b47c9834fd89a86da7c8e2c9334f987ffc8
Luca <luca.serfilippi@unibo.it>
2024-07-03 16:28:57 +0000

    perf script

--- run_experiments_mnist.sh
+++ run_experiments_mnist.sh
@@ -6,8 +6,11 @@ model_type=$1
 model_path="tflite_models/model_mnist_$model_type.tflite"
 
 #python
-python deploy_methods/python_code/python_tflite_mnist.py --model_path $model_path > mnist_test/tflite_python_inftime_mnist_$model_type.csv
-#wasm
-bash deploy_methods/wasm_code/mnist/br_wasm_mnist.sh $model_path $model_type
-#c++
-bash deploy_methods/c_code/tensorflow_lite_c/mnist/run_tensorflow_lite.sh $model_path $model_type
+python deploy_methods/python_code/python_tflite_mnist.py --model_path $model_path > mnist_test/tflite_python_inftime_mnist_$model_type.csv &
+
+# sudo perf record -p $(pgrep -f deploy_methods/python_code/python_tflite_mnist.py) -g -- sleep 30
+
+# #wasm
+# bash deploy_methods/wasm_code/mnist/br_wasm_mnist.sh $model_path $model_type
+# #c++
+# bash deploy_methods/c_code/tensorflow_lite_c/mnist/run_tensorflow_lite.sh $model_path $model_type
